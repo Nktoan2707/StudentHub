8be0f3fad88b3487ceaab9663b3a96dbdbc50b48
leesuby <62803507+leesuby@users.noreply.github.com>
2024-04-07 19:08:24 +0700

    fix minor

--- student_hub/lib/app.dart
+++ student_hub/lib/app.dart
@@ -56,7 +56,7 @@ class _AppState extends State<App> {
                 );
               } else if (state is AuthenticationAuthenticateFailure) {
                 _navigator.pushNamedAndRemoveUntil<void>(
-                  MainTabBarPage.pageId,
+                  HomePage.pageId,
                   (route) => false,
                 );
               }
